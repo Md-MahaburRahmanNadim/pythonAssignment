7bbc3c10c4067521e96745deb1741d92cff5d72f
unknown <mdnadimhaaan120@gmail.com>
2021-04-13 19:21:46 +0600

    all about bool(Ture or Flase) datatype.and real example

--- 3.prictualy bool datatype.py
+++ 3.prictualy bool datatype.py
@@ -12,12 +12,16 @@
 print( 4<5)
 print(' ') # it's use for new line
 print('Nadim' == 'nadim')
+# False becasue python is case sensctive language
 print(' ')
 print('nadim' == 'nadim')
+# Ture 
 print(' ')
 print('nadim' != 'nadim')
+# False because nadim is equal to nadim it's not not equal 
 print(' ')
 print('Nadim' != 'nadim')
+#Ture becuase Nadim is not equal to nadim so it's return ture
 
 print(' ')
 print('Nadim' > 'nadim')
