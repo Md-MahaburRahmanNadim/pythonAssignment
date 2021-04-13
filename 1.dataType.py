5d6e392f9469432bedf80d80edae88e54cc578b5
unknown <mdnadimhaaan120@gmail.com>
2021-04-13 16:24:34 +0600

    about int,float,bool,str data type in python

---
+++ 1.dataType.py
@@ -0,0 +1,28 @@
+# int 345; # any hole number
+#str 'Nadim', '34545'," Hasan 34er4"
+#bool True ,False, #is not to semelar the small letter true and false
+#float 34.4534 ,3434.3454 # desemal number
+
+#assignment . identifi the right variable
+
+
+# 'Hello' string 
+#123  int
+#4.54 float
+#True bool
+#"4" str
+#'3' str
+
+#variable declaration and variable writing some rule
+
+Name = 'value'
+NAME = 34
+name = 'nsdff'
+# above three variable are not same thing so python case sencitative
+#some rules
+#2name = 34 isn't not wright variable becuase it's start with number
+
+_name = 23
+#Name/ =455 isn't valide decleration because (/) it's mean in python is devision
+#age* = 23 also not valied decleration because (*) it's mean in python is multiplaction
+Hansn43wedklf = 'sdf'
