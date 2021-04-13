5096fa0fc395d12496241938189b8729270e1345
unknown <mdnadimhaaan120@gmail.com>
2021-04-13 19:20:44 +0600

    all about bool(Ture or Flase) datatype.and real example

---
+++ 3.prictualy bool datatype.py
@@ -0,0 +1,26 @@
+#<
+'''
+<=
+>=
+'''
+#>
+#==
+#!=
+# in python (=) single equal is use for variable decleration or store something
+# or return something
+
+print( 4<5)
+print(' ') # it's use for new line
+print('Nadim' == 'nadim')
+print(' ')
+print('nadim' == 'nadim')
+print(' ')
+print('nadim' != 'nadim')
+print(' ')
+print('Nadim' != 'nadim')
+
+print(' ')
+print('Nadim' > 'nadim')
+# in this condition here is not valied to comparied to the lenght of
+#string but it's compared to convert the asque code and than compared to each other
+# than it's reture the result
