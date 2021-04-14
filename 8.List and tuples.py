bc50a4b8f0a42b9a3898bcb4178aa87b723cfdc0
unknown <mdnadimhaaan120@gmail.com>
2021-04-14 09:57:32 +0600

    about list and how to appand lsit items and change exsiting list items

---
+++ 8.List and tuples.py
@@ -0,0 +1,31 @@
+'''
+list mean we can sture many data in one veriable also may type of data in one
+list
+'''
+friend = ['Mahamudul','Robiul',4346,'Shakib','Nadim']
+
+'''you can see here i include 2 data types in one list (str and int ). you can add float or bool data type what thing you need to compleate one task you 
+can do by using list. but you cant do this by using variable like x = 234,nadim,hasan . you have to assign 3 variable to store 3 differen type of data
+so here to much easy to use the list'''
+
+print(friend)
+
+print(' ')
+
+print(friend[0])
+''' to print or excess spacific item in the list we have to spacifi
+
+the indexing of this item
+'''
+'''
+to add new item in this existing list we use append() function like below'''
+print(' ')
+friend.append('Akash') #here we can only one value insert
+print(friend)
+
+print(' ')
+# to change exzisting item
+friend[2] = 'Shiblue'
+
+print(friend)
+
