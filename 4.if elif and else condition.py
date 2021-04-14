7ae791e5abf2538ab3589e41b00cc1779d70dfa0
unknown <mdnadimhaaan120@gmail.com>
2021-04-14 08:43:22 +0600

    some if else programing

---
+++ 4.if elif and else condition.py
@@ -0,0 +1,22 @@
+age = float(input('Enter your age'))
+
+#sentext of if contition
+#if condition:
+#    do this
+#elif condition:
+# do this
+
+if age < 16:
+    print('Your are younger')
+else:
+    print('your are elder ')
+
+# doing elif condition
+
+if age <=1:
+    print("you can't rid the rooler coster")
+elif age >= 2:
+    print("you can't rid the rooler coster")
+else:
+    print('you can ride happy')
+    
