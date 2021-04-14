05ffe8e86d746a32006e9e6e6a7bcaeb34200b9e
unknown <mdnadimhaaan120@gmail.com>
2021-04-14 10:52:39 +0600

    use of .strip(), .len(), .upper(), .lower() and .splet() but here must importent things is .splet() and .len function

---
+++ 10.len(), .strip(), .upper(), .lower(), .splet().py
@@ -0,0 +1,40 @@
+# .strip(), len(), .lower, .upper, .split()
+
+
+'''
+.strip() is use for to remove extra space after or befor variable
+'''
+
+text = input('Enter something with space ')
+print(text.strip())
+
+print(' ')
+
+'''
+len() to determin the lenth of any  string '''
+
+text = input('Enter something to determin length of the text : ')
+print(len(text))
+print(' ')
+
+'''
+.lower is use  to convert all upper case to lower case
+'''
+text = input(' Enter someting for lowering ')
+print(text.lower())
+print(' ')
+
+''' .upper is use to convert all lower case conver to upper case
+'''
+text = input(' Enter someting for upper all letter : ')
+print(text.upper())
+print(' ')
+
+'''
+must useable method in string is .split()
+.split() use for create list in python
+'''
+text = input(' Enter something to create list separet by . each item : ')
+print(text.split('.'))
+# here in braket we use . for separate each value
+# or item in list
