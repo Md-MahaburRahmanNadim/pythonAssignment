f5a303e1b19d01986fe5a59a4d0c6658ddbb8bf2
unknown <mdnadimhaaan120@gmail.com>
2021-04-14 09:08:05 +0600

    little bit about for loop syntex

---
+++ 6.for loop.py
@@ -0,0 +1,9 @@
+'''
+for loop syntext
+
+for variable_name in range(start,stop,step or increment or decrement):
+    print('sdfjdsjf')
+example is given below
+'''
+for x in range(0,12,5):
+    print(x)
