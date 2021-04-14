17b9f494327c9aaf89df279430cf2da070cb8867
unknown <mdnadimhaaan120@gmail.com>
2021-04-14 14:33:15 +0600

    How ot use MOdule in python and how to create self module in same dir

---
+++ 16.1Moduler Programing or how to use Module in python.py
@@ -0,0 +1,22 @@
+'''
+In pure python we have limited function but we need more function for that
+we have py build in function. Just we have to import those function in first
+line of programing . And also have many outer module in internet.IN pypi.org web site here 
+have lot's of module which open sourch. If we need
+we just have to download it by using pip module name 
+
+that's it
+'''
+#Buil in module in python 
+import math
+print(math.pi)
+print(' ')
+print(math.degrees(math.pi))
+print(' ')
+print(math.sqrt(4))
+
+# My create module in pyhon
+
+import 16.2SelfM as SM
+print(SM.Tsum(45,5))
+print(SM.Tsub(45,5))
---
+++ 16.2SelfM.py
@@ -0,0 +1,10 @@
+def Tsum(x,y):
+    Sum = x + y
+    print(Sum)
+    print(' ')
+    return 0
+def Tsub(x,y):
+    Sub = x - y
+    print(Sub)
+    return 0
+print(Tsum(45,34))
