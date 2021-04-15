a2e253574772529e2ef3124d434b215f065873e5
unknown <mdnadimhaaan120@gmail.com>
2021-04-15 06:11:58 +0600

    All about optional parameter in function

---
+++ 17.oprional parameter for function.py
@@ -0,0 +1,17 @@
+'''
+Optional parameter for function mean defult value for parameter for function
+
+It's really helpfull for us becuase we have many more parameter in a function
+for that may be forgot how many parameter given in those function for
+this sectiution if we put a defult value in a function than we don't have to
+worry about function parameter or argument
+
+'''
+def Abdullah(f = 'Bangla', W ='Arbi'):
+    print(f,W)
+    return 0
+Abdullah('ege', 'rice')
+
+Abdullah() # deful value
+# when we write our own MODULE than we have to space file the defult value of
+# our function parameter for symplecty
