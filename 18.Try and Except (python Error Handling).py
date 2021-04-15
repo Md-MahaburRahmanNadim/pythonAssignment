5a7f6b7446db86b9e777996b469f252c43993659
unknown <mdnadimhaaan120@gmail.com>
2021-04-15 06:14:58 +0600

    Error handling

---
+++ 18.Try and Except (python Error Handling).py
@@ -0,0 +1,30 @@
+'''
+By using try and except we handl our error on our programing . When we
+do roung sentext than python program crash . in this case we dont want
+that our program do crash instade of this we show except blog of code if
+program goes rong
+
+'''
+
+# like here we try to convert string to integer number but it's not possible
+'''
+text = 'Nadim'
+Converting = int(text)
+print(Converting)
+
+'''
+
+# if we run above blok of code we get an error massage but we don't want this
+'''
+if this(try) right than it's run otherwise except block of code run
+
+'''
+try:
+    text = 'Nadim'
+    
+
+
+    converting = int(text)
+    print(converting)
+except:
+    print('inviled sentext')
